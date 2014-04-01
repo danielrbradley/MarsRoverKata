5bcbf17798c6944a2f7d6d8685eb7b442e36ed4d
Daniel Bradley <daniel@danielbradley.net>
2014-04-01 22:08:53 +0100

    Create wrapping scenario tests

---
+++ MarsRover.UnitTests/UserInterface.WrappingScenarios.fs
@@ -0,0 +1,32 @@
+﻿module MarsRover.UnitTests.UserInterface.WrappingScenarios
+
+open NUnit.Framework
+open FsUnit
+open MarsRover.UserInterface
+open MarsRover.Domain
+open MarsRover.UnitTests.Vocabulary
+
+[<Test>]
+let ``Wrapping off bottom`` () =
+    ``On grid of`` (10,10) |> ``starting at`` (0,0) |> facing South
+    |> executing "F"
+    |> should equal "0,9 facing South"
+
+[<Test>]
+let ``Wrapping off top`` () =
+    ``On grid of`` (10,10) |> ``starting at`` (0,9) |> facing North
+    |> executing "F"
+    |> should equal "0,0 facing North"
+
+[<Test>]
+let ``Wrapping off left`` () =
+    ``On grid of`` (10,10) |> ``starting at`` (0,0) |> facing West
+    |> executing "F"
+    |> should equal "9,0 facing West"
+
+[<Test>]
+let ``Wrapping off right`` () =
+    ``On grid of`` (10,10) |> ``starting at`` (9,0) |> facing East
+    |> executing "F"
+    |> should equal "0,0 facing East"
+
