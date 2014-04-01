78438daa80a821f6b378629ba3fd9b4e25173a99
Daniel Bradley <daniel@danielbradley.net>
2014-04-01 23:24:07 +0100

    Reformat wrapping tests

--- MarsRover.UnitTests/Wrapping.fs
+++ MarsRover.UnitTests/Wrapping.fs
@@ -11,55 +11,56 @@ let ``Wrapping over``() = wrap -1 10 |> should equal 9
 [<Test>]
 let ``Wrapping under``() = wrap 10 10 |> should equal 0
 
-module Vocabulary =
-    let ``On 10x10 grid`` = 
-        { PosX = 0
-          PosY = 0
-          Heading = North
-          GridWidth = 10
-          GridHeight = 10 }
-
-    let ``starting at`` (x : int, y : int) (state : State) = 
-        { state with PosX = x
-                     PosY = y }
-
-    let facing (h : Heading) (state : State) = { state with Heading = h }
-
-    let executing (commands : string) (state : State) = 
-        state
-        |> execute (fun _ -> false) commands
-        |> sprint
-
-open Vocabulary
-
-[<Test>]
-let ``Wrapping off bottom``() = 
-    ``On 10x10 grid``
-    |> ``starting at`` (0, 0)
-    |> facing South
-    |> executing "F"
-    |> should equal "0,9 facing South"
-
-[<Test>]
-let ``Wrapping off top``() = 
-    ``On 10x10 grid``
-    |> ``starting at`` (0, 9)
-    |> facing North
-    |> executing "F"
-    |> should equal "0,0 facing North"
-
-[<Test>]
-let ``Wrapping off left``() = 
-    ``On 10x10 grid``
-    |> ``starting at`` (0, 0)
-    |> facing West
-    |> executing "F"
-    |> should equal "9,0 facing West"
-
-[<Test>]
-let ``Wrapping off right``() = 
-    ``On 10x10 grid``
-    |> ``starting at`` (9, 0)
-    |> facing East
-    |> executing "F"
-    |> should equal "0,0 facing East"
+module Scenarios = 
+    module Vocabulary = 
+        let ``On 10x10 grid`` = 
+            { PosX = 0
+              PosY = 0
+              Heading = North
+              GridWidth = 10
+              GridHeight = 10 }
+        
+        let ``starting at`` (x : int, y : int) (state : State) = 
+            { state with PosX = x
+                         PosY = y }
+        
+        let facing (h : Heading) (state : State) = { state with Heading = h }
+        
+        let executing (commands : string) (state : State) = 
+            state
+            |> execute (fun _ -> false) commands
+            |> sprint
+    
+    open Vocabulary
+    
+    [<Test>]
+    let ``Wrapping off bottom``() = 
+        ``On 10x10 grid``
+        |> ``starting at`` (0, 0)
+        |> facing South
+        |> executing "F"
+        |> should equal "0,9 facing South"
+    
+    [<Test>]
+    let ``Wrapping off top``() = 
+        ``On 10x10 grid``
+        |> ``starting at`` (0, 9)
+        |> facing North
+        |> executing "F"
+        |> should equal "0,0 facing North"
+    
+    [<Test>]
+    let ``Wrapping off left``() = 
+        ``On 10x10 grid``
+        |> ``starting at`` (0, 0)
+        |> facing West
+        |> executing "F"
+        |> should equal "9,0 facing West"
+    
+    [<Test>]
+    let ``Wrapping off right``() = 
+        ``On 10x10 grid``
+        |> ``starting at`` (9, 0)
+        |> facing East
+        |> executing "F"
+        |> should equal "0,0 facing East"
