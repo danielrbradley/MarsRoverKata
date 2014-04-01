0cae6d65433f9e311f6c860d762727ee4c7de9eb
Daniel Bradley <daniel@danielbradley.net>
2014-04-01 20:09:12 +0100

    Create command parser to interpret commands

---
+++ MarsRover.UnitTests/UserInterface.ParseCommand.fs
@@ -0,0 +1,23 @@
+﻿module MarsRover.UnitTests.UserInterface.ParseCommand
+
+open NUnit.Framework
+open FsUnit
+
+open MarsRover.UserInterface
+
+[<Test>]
+let ``Move forward`` ()=
+    parseCommand 'F' |> should equal MoveForward
+
+[<Test>]
+let ``Move backward`` ()=
+    parseCommand 'B' |> should equal MoveBackward
+
+[<Test>]
+let ``Turn left`` ()=
+    parseCommand 'L' |> should equal TurnLeft
+
+[<Test>]
+let ``Turn Right`` ()=
+    parseCommand 'R' |> should equal TurnRight
+
---
+++ MarsRover/UserInterface.fs
@@ -0,0 +1,15 @@
+﻿module MarsRover.UserInterface
+
+type Commands =
+    | MoveForward
+    | MoveBackward
+    | TurnLeft
+    | TurnRight
+
+let parseCommand (inputLetter : char) =
+    match inputLetter with
+    | 'F' -> MoveForward
+    | 'B' -> MoveBackward
+    | 'L' -> TurnLeft
+    | 'R' -> TurnRight
+    | _ -> failwith "Unrecognised command"
