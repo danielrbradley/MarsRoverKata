222768ce360e2d507e23ead92b2c85e5c04092dd
Daniel Bradley <daniel@danielbradley.net>
2014-04-01 23:27:32 +0100

    Reformat output tests

--- MarsRover.UnitTests/Output.fs
+++ MarsRover.UnitTests/Output.fs
@@ -6,23 +6,21 @@ open MarsRover.UserInterface
 open MarsRover.Domain
 
 [<Test>]
-let ``0,0 facing North``() =
-    let state = {
-        PosX = 0
-        PosY = 0
-        Heading = North
-        GridWidth = 10
-        GridHeight = 10
-    }
-    state |> sprint |> should equal "0,0 facing North"
+let ``0,0 facing North``() = 
+    { PosX = 0
+      PosY = 0
+      Heading = North
+      GridWidth = 10
+      GridHeight = 10 }
+    |> sprint
+    |> should equal "0,0 facing North"
 
 [<Test>]
-let ``3,1 facing East``() =
-    let state = {
-        PosX = 3
-        PosY = 1
-        Heading = East
-        GridWidth = 10
-        GridHeight = 10
-    }
-    state |> sprint |> should equal "3,1 facing East"
+let ``3,1 facing East``() = 
+    { PosX = 3
+      PosY = 1
+      Heading = East
+      GridWidth = 10
+      GridHeight = 10 }
+    |> sprint
+    |> should equal "3,1 facing East"
