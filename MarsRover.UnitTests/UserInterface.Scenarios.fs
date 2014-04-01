43bfbf3d78e4e3ad7807f7c92a7cbb6e1055810f
Daniel Bradley <daniel@danielbradley.net>
2014-04-01 21:13:26 +0100

    Create basic movement scenarios
     - Implement forward, back, left and right actions.

---
+++ MarsRover.UnitTests/UserInterface.Scenarios.fs
@@ -0,0 +1,142 @@
+﻿module MarsRover.UnitTests.UserInterface.Scenarios
+
+open NUnit.Framework
+open FsUnit
+open MarsRover.UserInterface
+open MarsRover.Domain
+
+module ``Starting at 1,1 facing North`` = 
+    let start = 
+        { PosX = 1
+          PosY = 1
+          Heading = North }
+    
+    [<Test>]
+    let ``Moving forward should increase y``() = 
+        start
+        |> execute "F"
+        |> sprint
+        |> should equal "1,2 facing North"
+    
+    [<Test>]
+    let ``Moving backward should decrease y``() = 
+        start
+        |> execute "B"
+        |> sprint
+        |> should equal "1,0 facing North"
+    
+    [<Test>]
+    let ``Turning right should head east``() = 
+        start
+        |> execute "R"
+        |> sprint
+        |> should equal "1,1 facing East"
+    
+    [<Test>]
+    let ``Turning left should head West``() = 
+        start
+        |> execute "L"
+        |> sprint
+        |> should equal "1,1 facing West"
+
+module ``Starting at 1,1 facing East`` = 
+    let start = 
+        { PosX = 1
+          PosY = 1
+          Heading = East }
+    
+    [<Test>]
+    let ``Moving forward should increase x``() = 
+        start
+        |> execute "F"
+        |> sprint
+        |> should equal "2,1 facing East"
+    
+    [<Test>]
+    let ``Moving backward should decrease x``() = 
+        start
+        |> execute "B"
+        |> sprint
+        |> should equal "0,1 facing East"
+    
+    [<Test>]
+    let ``Turning right should head South``() = 
+        start
+        |> execute "R"
+        |> sprint
+        |> should equal "1,1 facing South"
+    
+    [<Test>]
+    let ``Turning left should head North``() = 
+        start
+        |> execute "L"
+        |> sprint
+        |> should equal "1,1 facing North"
+
+module ``Starting at 1,1 facing South`` = 
+    let start = 
+        { PosX = 1
+          PosY = 1
+          Heading = South }
+    
+    [<Test>]
+    let ``Moving forward should decrease y``() = 
+        start
+        |> execute "F"
+        |> sprint
+        |> should equal "1,0 facing South"
+    
+    [<Test>]
+    let ``Moving backward should increase y``() = 
+        start
+        |> execute "B"
+        |> sprint
+        |> should equal "1,2 facing South"
+    
+    [<Test>]
+    let ``Turning right should head West``() = 
+        start
+        |> execute "R"
+        |> sprint
+        |> should equal "1,1 facing West"
+    
+    [<Test>]
+    let ``Turning left should head East``() = 
+        start
+        |> execute "L"
+        |> sprint
+        |> should equal "1,1 facing East"
+
+module ``Starting at 1,1 facing West`` = 
+    let start = 
+        { PosX = 1
+          PosY = 1
+          Heading = West }
+    
+    [<Test>]
+    let ``Moving forward should decrease x``() = 
+        start
+        |> execute "F"
+        |> sprint
+        |> should equal "0,1 facing West"
+    
+    [<Test>]
+    let ``Moving backward should increase x``() = 
+        start
+        |> execute "B"
+        |> sprint
+        |> should equal "2,1 facing West"
+    
+    [<Test>]
+    let ``Turning right should head North``() = 
+        start
+        |> execute "R"
+        |> sprint
+        |> should equal "1,1 facing North"
+    
+    [<Test>]
+    let ``Turning left should head South``() = 
+        start
+        |> execute "L"
+        |> sprint
+        |> should equal "1,1 facing South"
--- MarsRover/Domain.fs
+++ MarsRover/Domain.fs
@@ -11,14 +11,30 @@ type State =
       PosY : int
       Heading : Heading }
 
-let moveForward state =
-    state
+let moveForward state = 
+    match state.Heading with
+    | North -> { state with PosY = state.PosY + 1 }
+    | South -> { state with PosY = state.PosY - 1 }
+    | East -> { state with PosX = state.PosX + 1 }
+    | West -> { state with PosX = state.PosX - 1 }
 
-let moveBackward state =
-    state
+let moveBackward state = 
+    match state.Heading with
+    | North -> { state with PosY = state.PosY - 1 }
+    | South -> { state with PosY = state.PosY + 1 }
+    | East -> { state with PosX = state.PosX - 1 }
+    | West -> { state with PosX = state.PosX + 1 }
 
-let turnLeft state =
-    state
+let turnLeft state = 
+    match state.Heading with
+    | North -> { state with Heading = West }
+    | East -> { state with Heading = North }
+    | South -> { state with Heading = East }
+    | West -> { state with Heading = South }
 
-let turnRight state =
-    state
+let turnRight state = 
+    match state.Heading with
+    | North -> { state with Heading = East }
+    | East -> { state with Heading = South }
+    | South -> { state with Heading = West }
+    | West -> { state with Heading = North }
