251834b3019a7583ab5813790f02c2798f7d6b59
Daniel Bradley <daniel@danielbradley.net>
2014-04-02 00:08:41 +0100

    Create console application runner

---
+++ MarsRover/Application.fs
@@ -0,0 +1,42 @@
+﻿open System
+open MarsRover.Domain
+open MarsRover.Interface
+
+let execute = execute (fun _ -> false)
+
+let parseGridSize (input : string) = 
+    try 
+        let parts = input.Split('x') |> Array.map (fun x -> int32 x)
+        { PosX = 0
+          PosY = 0
+          Heading = North
+          GridWidth = parts.[0]
+          GridHeight = parts.[1] }
+    with _ -> 
+        { PosX = 0
+          PosY = 0
+          Heading = North
+          GridWidth = 10
+          GridHeight = 10 }
+
+let rec commandLoop (state : State) = 
+    Console.WriteLine(sprint state)
+    printf "Enter your next command (type exit to quit): \r\n"
+    let command = Console.ReadLine()
+    if command = "exit" then ()
+    else 
+        try 
+            state
+            |> execute command
+            |> commandLoop
+        with ex -> 
+            printf "Failed with %O" ex
+            commandLoop state
+
+let main() = 
+    printf "What size grid would you like? (Default is \"10x10\")\r\n"
+    let gridInput = Console.ReadLine()
+    let initialState = parseGridSize gridInput
+    commandLoop initialState
+
+main()
