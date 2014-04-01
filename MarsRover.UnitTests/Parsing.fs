6c8a5249d92df2966ef738955143daaa28d38259
Daniel Bradley <daniel@danielbradley.net>
2014-04-01 23:25:22 +0100

    Reformat parsing tests

--- MarsRover.UnitTests/Parsing.fs
+++ MarsRover.UnitTests/Parsing.fs
@@ -2,37 +2,33 @@
 
 open NUnit.Framework
 open FsUnit
-
 open MarsRover.UserInterface
 
-[<Test>]
-let ``Move forward`` ()=
-    parseCommand 'F' |> should equal MoveForward
-
-[<Test>]
-let ``Move backward`` ()=
-    parseCommand 'B' |> should equal MoveBackward
-
-[<Test>]
-let ``Turn left`` ()=
-    parseCommand 'L' |> should equal TurnLeft
-
-[<Test>]
-let ``Turn right`` ()=
-    parseCommand 'R' |> should equal TurnRight
-
-module ``Move forward then turn right`` =
-    let commands =  parseCommands "FR"
-
+module ``Single command`` = 
     [<Test>]
-    let ``the first command should be MoveForward`` () =
-       commands 
-       |> Seq.head 
-       |> should equal MoveForward
-
+    let ``Move forward``() = parseCommand 'F' |> should equal MoveForward
+    
     [<Test>]
-    let ``The second command should be TurnRight`` () =
-       commands 
-       |> Seq.skip 1
-       |> Seq.head 
-       |> should equal TurnRight
+    let ``Move backward``() = parseCommand 'B' |> should equal MoveBackward
+    
+    [<Test>]
+    let ``Turn left``() = parseCommand 'L' |> should equal TurnLeft
+    
+    [<Test>]
+    let ``Turn right``() = parseCommand 'R' |> should equal TurnRight
+
+module ``Comand sequence`` = 
+    let commands = parseCommands "FR"
+    
+    [<Test>]
+    let ``the first command should be MoveForward``() = 
+        commands
+        |> Seq.head
+        |> should equal MoveForward
+    
+    [<Test>]
+    let ``The second command should be TurnRight``() = 
+        commands
+        |> Seq.skip 1
+        |> Seq.head
+        |> should equal TurnRight
