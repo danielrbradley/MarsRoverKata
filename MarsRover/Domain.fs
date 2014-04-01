5450066b699294033d4c5f906dabccdb63ae9c07
Daniel Bradley <daniel@danielbradley.net>
2014-04-01 20:27:13 +0100

    Create initial domain outline
     - Stub command functions for now.

---
+++ MarsRover/Domain.fs
@@ -0,0 +1,24 @@
+﻿module MarsRover.Domain
+
+type Heading = 
+    | North
+    | South
+    | East
+    | West
+
+type State = 
+    { PosX : int
+      PosY : int
+      Heading : Heading }
+
+let moveForward state =
+    state
+
+let moveBackward state =
+    state
+
+let turnLeft state =
+    state
+
+let turnRight state =
+    state
